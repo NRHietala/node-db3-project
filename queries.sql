3a6fe6fd834037698c6f598f49d301e156fd4d7f
Nathan Hietala <Nrhietala@gmail.com>
2021-02-17 16:46:33 -0600

    Finished with SQL queries

--- queries.sql
+++ queries.sql
@@ -16,5 +16,19 @@ on o.shipvia = s.id
 where o.orderdate < "2012-08-09"
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+select
+o.OrderId, p.ProductName, o.quantity
+from orderdetail o
+join product p
+on o.productid = p.id
+where o.orderid = "10251"
+order by ProductName
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+select
+o.Id as OrderId, c.CompanyName,e.LastName
+from [order] o
+join employee e
+on o.employeeid = e.id
+join customer c
+on o.customerid = c.id
