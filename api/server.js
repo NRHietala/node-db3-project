90bea7f4b9ba22ee0d2d977d942604328e67b6d8
Nathan Hietala <Nrhietala@gmail.com>
2021-02-17 14:42:08 -0600

    Initial Commit/Setup, Server up w/catch all

--- api/server.js
+++ api/server.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
 
-const SchemeRouter = require('./schemes/scheme-router.js');
+const SchemeRouter = require("./schemes/scheme-router.js");
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+server.use("/api/schemes", SchemeRouter);
+
+server.get("/", (_, res) => {
+  res.send("Is your API running? You should go catch it! 🏃‍");
+});
+
+server.use("*", (_, res) => {
+  res.status(404).json({ message: "404: Resource not found" });
+});
 
 module.exports = server;
